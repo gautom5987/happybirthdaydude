2c8b9927da90b1deb2fd958168917edeaebbe0ea
Gautom Agarwal <garggautam72@gmail.com>
2021-06-23 23:02:34 +0530

    optimized for backend experiment

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+
+    <title>Happy Birthday (PERSON_NAME)!</title>
+    <meta name="description" content="Happy Birthday Dude!">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/small.css" media="(max-width: 630px)">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,700|Lobster' rel='stylesheet' type='text/css'>
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <div class="main_img">
+      <img src="(IMAGE FROM DB)" alt="Birthday image">
+      </div>
+
+      <section class="intro">
+        
+        <h2>(PERSON_NAME)'s Birthday Party!</h2>
+
+        <aside class="right">
+        <img src="(IMAGE FROM DB)" alt="Birthday boy/girl">
+        </aside>
+
+        <p> (TEXT FROM DB) </p>
+
+        <p> (TEXT FROM DB) </p>
+
+      </section>
+
+      <section class="contact">
+
+        <h2> (BIRTHDATE FROM DB) </h2>
+
+        <aside class="left">
+        <img src="(IMAGE FROM DB)" alt="">
+        </aside>
+
+        <p>(TEXT FROM DB)</p>
+
+        <p>(TEXT FROM DB)</p>
+          
+        <p>Coming? <a href="mailto:(MAIL ID FROM DB)?Subject=Yes I am coming">Email (MAIL ID FROM DB)</a></p>
+
+      </section>
+
+      <section class="endnote">
+        <p>(ENDNOTE FROM DB)</p>
+      </section>
+
+    </div>
+  </body>
+</html>
